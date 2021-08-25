9f61fcea2af21bc45a12c03354096eab0a9251de
Ken Preiser <kenpreiser@gmail.com>
2021-08-24 19:01:51 -0700

    Add files via upload

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,15 +1,3 @@
-var game = new Phaser.Game(400, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
-
-var PIECE_WIDTH = 200,
-    PIECE_HEIGHT = 200,
-    BOARD_COLS,
-    BOARD_ROWS;
-
-var piecesGroup,
-    piecesAmount,
-    shuffledIndexArray = [];
-
-
 // Application id from moralis.io
 Moralis.initialize('igDYNSIRB7leqHRpnUuNPfplY1fn0Y60fp5AM8zV');
 //Server url from moralis.io
@@ -17,173 +5,150 @@ Moralis.serverURL = 'https://e1qealego843.moralisweb3.com:2053/server';
 
 async function preload() {
     try {
-	    await ethereum.enable();
-        const user = await Moralis.Web3.authenticate();
-        console.log(user);
-        alert("User logged in");
-        
-        const userinfo = Moralis.User.current();
-        const userAddress = userinfo.get("ethAddress");
-        const openseaRugs = await fetch('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50&collection=8bit-rugs&owner=' + userAddress);
-        const rugs = await openseaRugs.json();
-        console.log(rugs);
-        hideLogin();
-   		game.load.spritesheet("background", rugs.assets[0].image_url, PIECE_WIDTH, PIECE_HEIGHT);
 
     } catch (error) {
         console.log(error);
     }
 }
 
-function create(rug) {
-    var piecesIndex = 0,
-        i, j,
-        piece;
-
-    BOARD_COLS = Math.floor(game.world.width / PIECE_WIDTH);
-    BOARD_ROWS = Math.floor(game.world.height / PIECE_HEIGHT);
-
-    piecesAmount = BOARD_COLS * BOARD_ROWS;
-
-    shuffledIndexArray = createShuffledIndexArray();
-
-    piecesGroup = game.add.group();
-
-    for (i = 0; i < BOARD_ROWS; i++)
-    {
-        for (j = 0; j < BOARD_COLS; j++)
-        {
-            if (shuffledIndexArray[piecesIndex]) {
-                piece = piecesGroup.create(j * PIECE_WIDTH, i * PIECE_HEIGHT, "background", shuffledIndexArray[piecesIndex]);
-            }
-            else { //initial position of black piece
-                piece = piecesGroup.create(j * PIECE_WIDTH, i * PIECE_HEIGHT);
-                piece.black = true;
-            }
-            piece.name = 'piece' + i.toString() + 'x' + j.toString();
-            piece.currentIndex = piecesIndex;
-            piece.destIndex = shuffledIndexArray[piecesIndex];
-            piece.inputEnabled = true;
-            piece.events.onInputDown.add(selectPiece, this);
-            piece.posX = j;
-            piece.posY = i;
-            piecesIndex++;
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
         }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
+};
 
-}
+var player;
+var stars;
+var platforms;
+var cursors;
+var score = 0;
+var scoreText;
 
-function selectPiece(piece) {
+var game = new Phaser.Game(config);
 
-    var blackPiece = canMove(piece);
+async function preload ()
+{
 
-    //if there is a black piece in neighborhood
-    if (blackPiece) {
-        movePiece(piece, blackPiece);
-    }
-
-}
-
-function canMove(piece) {
-
-    var foundBlackElem = false;
-
-    piecesGroup.children.forEach(function(element) {
-        if (element.posX === (piece.posX - 1) && element.posY === piece.posY && element.black ||
-            element.posX === (piece.posX + 1) && element.posY === piece.posY && element.black ||
-            element.posY === (piece.posY - 1) && element.posX === piece.posX && element.black ||
-            element.posY === (piece.posY + 1) && element.posX === piece.posX && element.black) {
-            foundBlackElem = element;
-            return;
-        }
-    });
-
-    return foundBlackElem;
-}
-
-function movePiece(piece, blackPiece) {
-
-    var tmpPiece = {
-        posX: piece.posX,
-        posY: piece.posY,
-        currentIndex: piece.currentIndex
-    };
-
-    game.add.tween(piece).to({x: blackPiece.posX * PIECE_WIDTH, y: blackPiece.posY * PIECE_HEIGHT}, 300, Phaser.Easing.Linear.None, true);
-
-    //change places of piece and blackPiece
-    piece.posX = blackPiece.posX;
-    piece.posY = blackPiece.posY;
-    piece.currentIndex = blackPiece.currentIndex;
-    piece.name ='piece' + piece.posX.toString() + 'x' + piece.posY.toString();
-
-    //piece is the new black
-    blackPiece.posX = tmpPiece.posX;
-    blackPiece.posY = tmpPiece.posY;
-    blackPiece.currentIndex = tmpPiece.currentIndex;
-    blackPiece.name ='piece' + blackPiece.posX.toString() + 'x' + blackPiece.posY.toString();
-
-    //after every move check if puzzle is completed
-    checkIfFinished();
-}
-
-function checkIfFinished() {
-
-    var isFinished = true;
-
-    piecesGroup.children.forEach(function(element) {
-        if (element.currentIndex !== element.destIndex) {
-            isFinished = false;
-            return;
-        }
-    });
-
-    if (isFinished) {
-        showFinishedText();
-    }
-
-}
-
-function showFinishedText() {
-
-    var style = { font: "40px Arial", fill: "#000", align: "center"};
-
-    var text = game.add.text(game.world.centerX, game.world.centerY, "Congratulations! \nYou made it!", style);
-
-    text.anchor.set(0.5);
-
-}
-
-function createShuffledIndexArray() {
-
-    var indexArray = [];
-
-    for (var i = 0; i < piecesAmount; i++)
-    {
-        indexArray.push(i);
-    }
-
-    return shuffle(indexArray);
-
-}
-
-function shuffle(array) {
-
-    var counter = array.length,
-        temp,
-        index;
-
-    while (counter > 0)
-    {
-        index = Math.floor(Math.random() * counter);
-
-        counter--;
-
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-
-    return array;
+    await ethereum.enable();
+    const user = await Moralis.Web3.authenticate();
+    console.log(user);
+    alert("User logged in");
     
+    const userinfo = Moralis.User.current();
+    const userAddress = userinfo.get("ethAddress");
+    const openseaRugs = await fetch('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50&collection=8bit-rugs&owner=' + userAddress);
+    const rugs = await openseaRugs.json();
+    console.log(rugs);
+
+    this.load.image('sky', '../img//sky.jpg');
+    this.load.image('ground', '../img/platform.png');
+    this.load.image('star', '../img/star.png');
+    this.load.image('bomb', '../img/bomb.png');
+    this.load.spritesheet('dude', rugs.assets[0].image_url, { frameWidth: 32, frameHeight: 48 });
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+    });
+
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+}
+
+function update ()
+{
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
+}
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
 }
